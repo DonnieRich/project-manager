410634dce64de9386bb6cd8aa98ad3878a41dff1
Donnie <donniepask@gmail.com>
2023-09-19 15:43:26 +0200

    Added Project model factory to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Project::factory()->count(5)->create();
     }
 }
